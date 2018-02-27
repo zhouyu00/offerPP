41b43b673ef8a0021541a78b2a89204c6ff2e3d0
L1ANN <342141672@qq.com>
2018-02-27 09:34:37 +0800

    增加了UserDao的实现 新增用于发送邮件的工具类JavaEmailSender 修改了c3p0池的连接上限 建立了对UserDao的单元测试 增加了js，css，fonts文件夹 maven中增加对javamail的依赖

--- src/main/java/team/offer/dao/UserDao.java
+++ src/main/java/team/offer/dao/UserDao.java
@@ -1,5 +1,6 @@
 package team.offer.dao;
 
+import org.apache.ibatis.annotations.Param;
 import team.offer.entity.Position;
 import team.offer.entity.User;
 
@@ -12,45 +13,46 @@ import java.util.List;
  * @Modified By:
  */
 public interface UserDao {
+
     //根据用户id查询用户
-    User queryUserById(Integer id) throws Exception;
+    User queryUserById(@Param("id")Integer id) throws Exception;
 
     //根据邮箱获取用户信息
-    User queryUserByEmail(String email) throws Exception;
+    User queryUserByEmail(@Param("email")String email) throws Exception;
 
     //增加一条用户记录
     void insertUser(User user) throws Exception;
 
     //根据邮箱和密码查询用户
-    User queryUserByEmailAndPassword(String email,String password) throws Exception;
+    User queryUserByEmailAndPassword(@Param("email")String email,@Param("password")String password) throws Exception;
 
     //根据id修改用户密码
-    void updateUserPasswordById(Integer id,String password) throws Exception;
+    void updateUserPasswordById(@Param("id")Integer id, @Param("password")String password) throws Exception;
 
     //根据id查询用户收藏的职位
-    List<Position> queryCollectionById(Integer id) throws Exception;
+    List<Position> queryCollectionById(@Param("id")Integer id) throws Exception;
 
     //根据职位id删除用户收藏的职位
-    void deleteCollectionById(Integer id) throws Exception;
+    void deleteCollectionById(@Param("uid")Integer uid,@Param("pid")Integer pid) throws Exception;
 
     //根据用户id查询新投递的职位
-    List<Position> queryNewApplicationById(Integer id) throws Exception;
+    List<Position> queryNewApplicationById(@Param("id")Integer id) throws Exception;
 
     //根据用户id查询已查阅的职位
-    List<Position> queryConsultedApplicationById(Integer id) throws Exception;
+    List<Position> queryConsultedApplicationById(@Param("id")Integer id) throws Exception;
 
     //根据用户id查询待安排的职位
-    List<Position> queryArrangedApplicationById(Integer id) throws Exception;
+    List<Position> queryArrangedApplicationById(@Param("id")Integer id) throws Exception;
 
     //根据用户id和职位id查询申请id
-    Integer queryApplicationIdByUidAndPid(Integer uid,Integer pid) throws Exception;
+    Integer queryApplicationIdByUidAndPid(@Param("uid")Integer uid,@Param("pid")Integer pid) throws Exception;
 
     //增加一条申请表记录
-    void insertApplication(Integer uid,Integer pid) throws Exception;
+    void insertApplication(@Param("uid")Integer uid,@Param("pid")Integer pid) throws Exception;
 
     //根据用户id和职位id查询收藏id
-    Integer queryCollectionIdByUidAndPid(Integer uid,Integer pid) throws Exception;
+    Integer queryCollectionIdByUidAndPid(@Param("uid")Integer uid,@Param("pid")Integer pid) throws Exception;
 
     //增加一条收藏表记录
-    void insertCollection(Integer uid,Integer pid) throws Exception;
+    void insertCollection(@Param("uid")Integer uid,@Param("pid")Integer pid) throws Exception;
 }
---
+++ src/main/webapp/WEB-INF/jsp/user/register.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: L1ANN
+  Date: 2018/2/26
+  Time: 9:15
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>注册</title>
+</head>
+<body>
+<form
+</body>
+</html>
